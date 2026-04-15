ad3c5971e6c9d892db868d6e668a2eee00f137e2
Pratul <Pratuld@mindfiresolutions.com>
2026-04-15 22:38:41 +0530

    Initial Commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by Pratul Dawande on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
